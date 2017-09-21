b5ffe67382d7b34c62391923bf4523ecb59e91fe
DESKTOP-5D1H62T\User <chid2539@icloud.com>
2017-09-21 21:07:08 +0700

    คนสวย อัพเดท

---
+++ src/com/rs/update.java
@@ -0,0 +1,46 @@
+package com.rs;
+
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.bson.types.ObjectId;
+
+import com.connect.connect;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+
+@Path("/update")
+public class update {
+	@POST
+	@Path("/update") 
+	@Produces(MediaType.TEXT_PLAIN)  
+	public String updates( @FormParam("name") String name, @FormParam("version") String version, @FormParam("type") String type,@FormParam("price") String price,
+			@FormParam("day") String day, @FormParam("charge") String charge,@FormParam("total") String total) {   
+		
+		DB db = new connect().mongo();
+		DBCollection collection = db.getCollection("course");
+		
+		BasicDBObject document = new BasicDBObject();
+		document.put("name", name);
+		document.put("version", version);
+		document.put("type", type);
+		document.put("price", price);
+		document.put("day", day);
+		document.put("charge", charge);
+		document.put("total", total);
+		
+		BasicDBObject setQuery = new BasicDBObject();
+        setQuery.put("$set", document);
+		
+		BasicDBObject searchQuery = new BasicDBObject();
+		searchQuery.put("name", new ObjectId(name));
+
+		collection.update(searchQuery, setQuery);
+		
+		return "true";  
+	} 
+}
