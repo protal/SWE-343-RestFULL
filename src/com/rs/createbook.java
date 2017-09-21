abf2697fbd345641644986aefac4276e3ced5d3f
THASAWAT\Thasawat <rthasawat@gmaill.com>
2017-09-21 20:24:07 +0700

    add path

--- src/com/rs/createbook.java
+++ src/com/rs/createbook.java
@@ -16,7 +16,7 @@ import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
-
+@Path("/create") 
 public class createbook {
 	@POST
 	@Path("/create") 
